a218d9196c7cb577e01585233aedb9de0914d401
Daniel Barclay <barclaysd@me.com>
2018-12-19 20:22:21 +0000

    useEffect to retrieve stored data in Firebase

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,42 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const todo = props => {
-   // const [todoName, setTodoName] = useState('');
-   // const [todoList, setTodoList] = useState([]);
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
 
-    const [todoState, setTodoState] = useState({
-        userInput: '',
-        todoList: []
+    useEffect(() => {
+        axios.get('https://react-hooks-e0025.firebaseio.com/todos.json')
+            .then(response => {
+                console.log(response.data);
+                const todoData = response.data;
+                let todos = [];
+                for (const key in todoData) {
+                    todos.push({id: key, name: todoData[key].name});
+                }
+                setTodoList(todos);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     });
 
-   const inputChangeHandler = (event) => {
-       setTodoState({
-           userInput: event.target.value,
-            todoList: todoState.todoList
-       });
+    const inputChangeHandler = (event) => {
+        setTodoName(event.target.value);
     };
 
-   const todoAddHandler = () => {
-       setTodoState({
-           userInput: todoState.userInput,
-            todoList: todoState.todoList.concat(todoState.userInput)
-       });
-   };
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
+        axios.post('https://react-hooks-e0025.firebaseio.com/todos.json', {name: todoName})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        setTodoName('');
+    };
 
     return (
-    <React.Fragment>
-        <input
-            type='text'
-            placeholder='Todo'
-            value={todoState.userInput}
-            onChange={inputChangeHandler}/>
-        <button type='button' onClick={todoAddHandler}>Add</button>
-        <ul>
-            {todoState.todoList.map((todo, index) => (
-                <li key={index}>{todo}</li>
+        <React.Fragment>
+            <input
+                type='text'
+                placeholder='Todo'
+                value={todoName}
+                onChange={inputChangeHandler}/>
+            <button type='button' onClick={todoAddHandler}>Add</button>
+            <ul>
+                {todoList.map(todo => (
+                    <li key={todo.id}>{todo.name}</li>
                 ))}
-        </ul>
-    </React.Fragment>
+            </ul>
+        </React.Fragment>
     );
 
 };
